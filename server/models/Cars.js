ae414d99e705a9688dbacc4acb1a67638c105b02
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-10-05 19:45:06 -0400

    Added images to assets and added footer starter code. Footer has team repo and team personal portfolio links.

---
+++ server/models/Cars.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+  purchaseDate: {
+    type: Date,
+    default: Date.now
+  },
+  products: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Product'
+    }
+  ]
