4e6cca590a3c46e26dbf90d737e2af4f37ddf18e
Ronin1702 <kchen@me.com>
2023-10-13 05:16:50 -0400

    add View Orders dom link to RENT A PRO page

--- client/src/pages/Pros.jsx
+++ client/src/pages/Pros.jsx
@@ -1,11 +1,18 @@
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
+import ProductList from '../components/ProductList';
+import CategoryMenu from '../components/CategoryMenu';
+import { Link } from 'react-router-dom';
 
 const RentAPro = () => {
   return (
-    <div className="container">
+    <div className='container'>
+      <div className='row mt-2 text-end fs-6'>
+        <Link className='text-secondary' to='/OrderHistory'>
+          {' '}
+          📖 View Orders{' '}
+        </Link>
+      </div>
       <CategoryMenu />
-      <hr/>
+      <hr />
       <ProductList />
     </div>
   );
