a9da814681e5199ffc1a0f32c3aed4d84a5d2c2d
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-10-14 23:38:57 -0400

    add SVG rendering to ComplaintResults.jsx component

--- client/src/components/renderMake.jsx
+++ client/src/components/renderMake.jsx
@@ -41,256 +41,262 @@ import toyota from '../assets/logos/toyota.svg';
 import volkswagen from '../assets/logos/volkswagen.svg';
 import volvo from '../assets/logos/volvo.svg';
 
+// import React, { useState } from 'react';
 
 
-const MakeLogo = ({ makeRef }) => {
-    if (makeRef === 'acura') {
+
+// const carMake = useState(localStorage.getItem('make') || '');
+
+
+
+const MakeLogo = ({ carMake }) => {
+    if (carMake === 'acura') {
         return (
             <div>
                 <img src={acura} alt='Acura Logo'></img>
             </div>
         );
-    } else if (makeRef === 'alfaromeo') {
+    } else if (carMake === 'alfaromeo') {
         return (
             <div>
                 <img src={alfaromeo} alt='Alfa Romeo Logo'></img>
             </div>
         );
-    } else if (makeRef === 'astonmartin') {
+    } else if (carMake === 'astonmartin') {
         return (
             <div>
                 <img src={astonmartin} alt='Aston Martin Logo'></img>
             </div>
         );
-    } else if (makeRef === 'audi') {
+    } else if (carMake === 'audi') {
         return (
             <div>
                 <img src={audi} alt='Audi Logo'></img>
             </div>
         );
-    } else if (makeRef === 'bentley') {
+    } else if (carMake === 'bentley') {
         return (
             <div>
                 <img src={bentley} alt='Bentley Logo'></img>
             </div>
         );
-    } else if (makeRef === 'bmw') {
+    } else if (carMake === 'bmw') {
         return (
             <div>
                 <img src={bmw} alt='BMW Logo'></img>
             </div>
         );
-    } else if (makeRef === 'bugatti') {
+    } else if (carMake === 'bugatti') {
         return (
             <div>
                 <img src={bugatti} alt='Bugatti Logo'></img>
             </div>
         );
-    } else if (makeRef === 'buick') {
+    } else if (carMake === 'buick') {
         return (
             <div>
                 <img src={buick} alt='Buick Logo'></img>
             </div>
         );
-    } else if (makeRef === 'cadillac') {
+    } else if (carMake === 'cadillac') {
         return (
             <div>
                 <img src={cadillac} alt='Cadillac Logo'></img>
             </div>
         );
-    } else if (makeRef === 'chevrolet') {
+    } else if (carMake === 'chevrolet') {
         return (
             <div>
                 <img src={chevrolet} alt='Chevrolet Logo'></img>
             </div>
         );
-    } else if (makeRef === 'chrysler') {
+    } else if (carMake === 'chrysler') {
         return (
             <div>
                 <img src={chrysler} alt='Chrysler Logo'></img>
             </div>
         );
-    } else if (makeRef === 'dodge') {
+    } else if (carMake === 'dodge') {
         return (
             <div>
                 <img src={dodge} alt='Dodge Logo'></img>
             </div>
         );
-    } else if (makeRef === 'ferrari') {
+    } else if (carMake === 'ferrari') {
         return (
             <div>
                 <img src={ferrari} alt='Ferrari Logo'></img>
             </div>
         );
-    } else if (makeRef === 'fiat') {
+    } else if (carMake === 'fiat') {
         return (
             <div>
                 <img src={fiat} alt='Fiat Logo'></img>
             </div>
         );
-    } else if (makeRef === 'ford') {
+    } else if (carMake === 'ford') {
         return (
             <div>
                 <img src={ford} alt='Ford Logo'></img>
             </div>
         );
-    } else if (makeRef === 'gmc') {
+    } else if (carMake === 'gmc') {
         return (
             <div>
                 <img src={gmc} alt='GMC Logo'></img>
             </div>
         );
-    } else if (makeRef === 'honda') {
+    } else if (carMake === 'honda') {
         return (
             <div>
                 <img src={honda} alt='Honda Logo'></img>
             </div>
         );
-    } else if (makeRef === 'hummer') {
+    } else if (carMake === 'hummer') {
         return (
             <div>
                 <img src={hummer} alt='Hummer Logo'></img>
             </div>
         );
-    } else if (makeRef === 'hyundai') {
+    } else if (carMake === 'hyundai') {
         return (
             <div>
                 <img src={hyundai} alt='Hyundai Logo'></img>
             </div>
         );
-    } else if (makeRef === 'infiniti') {
+    } else if (carMake === 'infiniti') {
         return (
             <div>
                 <img src={infiniti} alt='Infiniti Logo'></img>
             </div>
         );
-    } else if (makeRef === 'jaguar') {
+    } else if (carMake === 'jaguar') {
         return (
             <div>
                 <img src={jaguar} alt='Jaguar Logo'></img>
             </div>
         );
-    } else if (makeRef === 'jeep') {
+    } else if (carMake === 'jeep') {
         return (
             <div>
                 <img src={jeep} alt='Jeep Logo'></img>
             </div>
         );
-    } else if (makeRef === 'kia') {
+    } else if (carMake === 'kia') {
         return (
             <div>
                 <img src={kia} alt='Kia Logo'></img>
             </div>
         );
-    } else if (makeRef === 'lamborghini') {
+    } else if (carMake === 'lamborghini') {
         return (
             <div>
                 <img src={lamborghini} alt='Lamborghini Logo'></img>
             </div>
         );
-    } else if (makeRef === 'landrover') {
+    } else if (carMake === 'landrover') {
         return (
             <div>
                 <img src={landrover} alt='Land Rover Logo'></img>
             </div>
         );
-    } else if (makeRef === 'lexus') {
+    } else if (carMake === 'lexus') {
         return (
             <div>
                 <img src={lexus} alt='Lexus Logo'></img>
             </div>
         );
-    } else if (makeRef === 'lincoln') {
+    } else if (carMake === 'lincoln') {
         return (
             <div>
                 <img src={lincoln} alt='Lincoln Logo'></img>
             </div>
         );
-    } else if (makeRef === 'lotus') {
+    } else if (carMake === 'lotus') {
         return (
             <div>
                 <img src={lotus} alt='Lotus Logo'></img>
             </div>
         )
-    } else if (makeRef === 'maserati') {
+    } else if (carMake === 'maserati') {
         return (
             <div>
                 <img src={maserati} alt='Maserati Logo'></img>
             </div>
         )
-    } else if (makeRef === 'mazda') {
+    } else if (carMake === 'mazda') {
         return (
             <div>
                 <img src={mazda} alt='Mazda Logo'></img>
             </div>
         );
-    } else if (makeRef === 'mercedesbenz') {
+    } else if (carMake === 'mercedesbenz') {
         return (
             <div>
                 <img src={mercedesbenz} alt='Mercedes-Benz Logo'></img>
             </div>
         );
-    } else if (makeRef === 'mercury') {
+    } else if (carMake === 'mercury') {
         return (
             <div>
                 <img src={mercury} alt='Mercury Logo'></img>
             </div>
         );
-    } else if (makeRef === 'mitsubishi') {
+    } else if (carMake === 'mitsubishi') {
         return (
             <div>
                 <img src={mitsubishi} alt='Mitsubishi Logo'></img>
             </div>
         );
-    } else if (makeRef === 'nissan') {
+    } else if (carMake === 'nissan') {
         return (
             <div>
                 <img src={nissan} alt='Nissan Logo'></img>
             </div>
         );
-    } else if (makeRef === 'pontiac') {
+    } else if (carMake === 'pontiac') {
         return (
             <div>
                 <img src={pontiac} alt='Pontiac Logo'></img>
             </div>
         );
-    } else if (makeRef === 'porsche') {
+    } else if (carMake === 'porsche') {
         return (
             <div>
                 <img src={porsche} alt='Porsche Logo'></img>
             </div>
         );
-    } else if (makeRef === 'rollsroyce') {
+    } else if (carMake === 'rollsroyce') {
         return (
             <div>
                 <img src={rollsroyce} alt='Rolls Royce Logo'></img>
             </div>
         );
-    } else if (makeRef === 'subaru') {
+    } else if (carMake === 'subaru') {
         return (
             <div>
                 <img src={subaru} alt='Subaru Logo'></img>
             </div>
         );
-    } else if (makeRef === 'tesla') {
+    } else if (carMake === 'tesla') {
         return (
             <div>
                 <img src={tesla} alt='Tesla Logo'></img>
             </div>
         );
-    } else if (makeRef === 'toyota') {
+    } else if (carMake === 'toyota') {
         return (
             <div>
                 <img src={toyota} alt='Toyota Logo'></img>
             </div>
         );
-    } else if (makeRef === 'volkswagen') {
+    } else if (carMake === 'volkswagen') {
         return (
             <div>
                 <img src={volkswagen} alt='Volkswagen Logo'></img>
             </div>
         );
-    } else if (makeRef === 'volvo') {
+    } else if (carMake === 'volvo') {
         return (
             <div>
                 <img src={volvo} alt='Volvo Logo'></img>
